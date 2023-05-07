ee0fb569a3b5be62a0881007f73a128c2fc9cfa1
satzbe <satzb.e.17@gmail.com>
2023-05-08 00:16:35 +0530

    css configuration added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['.index.html', './src/**/*.{js,ts,tsx,jsx}'],
+  content: [".index.html", "./src/**/*.{js,ts,tsx,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        bounce200: "bounce 1s infinite 200ms",
+        bounce400: "bounce 1s infinite 400ms",
+      },
+    },
     colors: {
-      mainColor: '#1597E4',
-      borderClr: '#E6E6E6',
-      errorClr: '#D86161',
-      plh: '#7A7A7A',
-      darkFont: '#212121',
-      lightFont: '#FAFAFA',
-      backgroundClr: '#F5F5F5',
-      default: 'white',
+      mainColor: "#1597E4",
+      borderClr: "#E6E6E6",
+      errorClr: "#D86161",
+      plh: "#7A7A7A",
+      darkFont: "#212121",
+      lightFont: "#FAFAFA",
+      backgroundClr: "#F5F5F5",
+      default: "white",
     },
   },
   plugins: [],
